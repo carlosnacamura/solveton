15de00416ef2d0e4a547491a573354d80c2f67f9
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2024-05-12 19:52:24 +0000

    Teste navbar/ Bug

--- js/keywords.js
+++ js/keywords.js
@@ -1,10 +1,12 @@
 const keywords = document.querySelectorAll(".keywords");
-const colors = ["blue", "tomato", "green"];
+const colors = ["#9cf5ff", "#9ccdff", "#b0baff"];
 let index = 0;
 setInterval(() => {
+  keywords[index].classList.toggle("keywordsFeature");
   keywords[index].style.color = colors[index];
-  setTimeout(() => {
-    keywords[index].style.color = "black";
-    index = (index + 1) % colors.length;
-  }, 2000);
+}, 2000);
+setInterval(() => {
+  keywords[index].style.color = "#a0b0c2"
+  index = (index + 1) % colors.length;
 }, 4000);
+// Achei que ia ser fácil....
--- js/navbarScroll.js
+++ js/navbarScroll.js
@@ -3,4 +3,5 @@ const navbar = document.querySelector(".navbar");
 addEventListener("scroll", () => {
   const y = window.scrollY;
   navbar.classList.toggle("navbarFix", y >= 685);
+  navbar.classList.toggle("navbarVanish", y < 685)
 });
