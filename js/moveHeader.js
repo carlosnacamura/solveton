0f249f9e40011877db89669570dfdfc829c03939
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2024-05-13 14:36:49 +0000

    Varias modificações

--- js/moveHeader.js
+++ js/moveHeader.js
@@ -1,9 +1,18 @@
-document.addEventListener('mousemove', parallax);
-
-function parallax(e) {
-    const move = document.querySelector('.header-content');
+document.addEventListener('mousemove', parallax_header);
+document.addEventListener('mousemove',parallax_circle)
+function parallax_header(e) {
+    const move = document.querySelector(".header-content")
     var moving_value = move.getAttribute('data-value');
-    var x = (e.clientX - window.innerWidth / 2) / moving_value * 0.1;
-    var y = (e.clientY - window.innerHeight / 2) / moving_value * 0.1;
-    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    var x_header = (e.clientX - window.innerWidth / 2) / moving_value * 0.1;
+    var y_header = (e.clientY - window.innerHeight / 2) / moving_value * 0.1;
+    move.style.transform = "translateX(" + x_header + "px) translateY(" + y_header + "px)";
+}
+function parallax_circle(e){
+    const circles = document.querySelectorAll(".circle_header")
+    circles.forEach(circle => {
+    var moving_value_circle = circle.getAttribute('data-value');
+    var x_circle = (e.clientX - window.innerWidth / 2) / moving_value_circle * 0.01;
+    var y_circle = (e.clientY - window.innerHeight / 2) / moving_value_circle * 0.01;
+    circle.style.transform = "translateX(" + x_circle + "px) translateY(" + y_circle + "px)";
+    });
 }
